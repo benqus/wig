80958f5844a1385082f47493f6d37765a8132b0c
Bence Kormos <bence.kormos@hogarthww.com>
2015-01-17 21:59:19 +0000

    deployment script cleans up before committing

--- copy_dev_to_master.sh
+++ copy_dev_to_master.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+# cleanup workspace
+git clean -f -d
+
 # check out master
 git checkout master
 
 # check out latest files from dev
 git checkout dev LICENSE
 git checkout dev README.md
+git checkout dev .gitignore
 git checkout dev .jshintrc
+git checkout dev Gruntfile.js
 git checkout dev package.json
 git checkout dev wig.js
 
 # commit latest version of the files in
 git add .
-git commit -m "Update latest version"
+git commit -m "Update latest version by $( cat ./.git/refs/heads/dev )"
 git push
