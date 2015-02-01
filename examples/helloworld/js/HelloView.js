89087999ae76d204ddfba5f819a145e77d057b2a
Bence Kormos <bence.kormos@hogarthww.com>
2015-02-01 01:14:40 +0000

    fixes css class generation and adss hello worl example

---
+++ examples/helloworld/js/HelloView.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+
+    // create a new View class
+    var HelloView = wig.View.extend({
+        tagName: 'h1',
+        className: 'HelloView',
+        template: 'Hello {{ name }}!'
+    });
+
+    // instantiate a new View instance with a context
+    var view = new HelloView({
+        name: 'Doug'
+    });
+
+    window.onload = function () {
+        // render the View instance
+        wig.renderView(view, document.body);
+    };
+}());
